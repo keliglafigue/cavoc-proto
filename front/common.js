fdfa4b51b1c54990310cb3d830a03a56eab16992
Kélig SOARES--QUEBRIAC <kelig.soares--quebriac@etu.univ-nantes.fr>
2026-06-05 10:20:09 +0200

    Display hint at beginning

--- front/common.js
+++ front/common.js
@@ -7,7 +7,15 @@ function closeModal(modal) {
 }
 
 function setModalTitle(modal, title) {
-    modal.getElementsByClassName('win-title')[0].innerHTML = title;
+    let titleTag = modal.getElementsByClassName('win-title');
+    if (titleTag.length === 0) {
+        const newTitleTag = document.createElement('h1');
+        newTitleTag.classList.add('win-title');
+        newTitleTag.innerHTML = title;
+        modal.getElementsByClassName('win-modal-content')[0].prepend(newTitleTag);
+    }
+
+    else titleTag[0].innerHTML = title;
 }
 
 function setModalMessage(modal, message) {
@@ -28,14 +36,14 @@ function addModalButton(modal, text, onClick) {
 function createModal(id, withCloseButton) {
     const modal   = document.createElement('div');
     const content = document.createElement('div');
-    const title   = document.createElement('h2');
+    // const title   = document.createElement('h2');
     const par     = document.createElement('p');
 
     modal.id = id;
 
     modal.classList.add('win-modal');
     content.classList.add('win-modal-content');
-    title.classList.add('win-title');
+    // title.classList.add('win-title');
     par.classList.add('win-message');
 
     if(withCloseButton) {
@@ -47,7 +55,7 @@ function createModal(id, withCloseButton) {
     }
 
     modal.appendChild(content);
-    content.appendChild(title);
+    // content.appendChild(title);
     content.appendChild(par);
 
     modal.style = 'display: none';
